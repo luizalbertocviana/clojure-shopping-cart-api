2277284b31de7c5f327e121fbe3075d20b325469
Luiz Alberto do Carmo Viana <luizalbertocviana@gmail.com>
2022-07-13 16:57:15 -0300

    first version of `database.clj`

---
+++ src/app/services/database.clj
@@ -0,0 +1,20 @@
+(ns app.services.database
+  (:require [hikari-cp.core :as cp]
+            [clojure.java.jdbc :as jdbc]
+            [integrant.core :as ig]))
+
+(defmethod ig/init-key ::connection
+  [_ datasource-options]
+  {:datasource (cp/make-datasource datasource-options)})
+
+(defmethod ig/init-key ::querier
+  [_ {:keys [db-connection]}]
+  (fn [sql-params]
+    (jdbc/query db-connection sql-params)))
+
+(defmethod ig/init-key ::transactor
+  [_ {:keys [db-connection]}]
+  (fn [sql-params-seq]
+    (jdbc/with-db-transaction [t-conn db-connection]
+      (for [sql-params sql-params-seq]
+        (jdbc/execute! t-conn sql-params)))))
