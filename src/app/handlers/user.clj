b03fc964508d2fec4bbb701bf3d2ffa4ef4895b5
Luiz Alberto do Carmo Viana <luizalbertocviana@gmail.com>
2022-07-19 10:56:46 -0300

    creates user login handler

--- src/app/handlers/user.clj
+++ src/app/handlers/user.clj
@@ -14,6 +14,18 @@
           :id)
       nil)))
 
+(defn active-session [querier user-id]
+  (let [query {:select [:id]
+               :from [:sessions]
+               :where [:and [:= :user-id user-id]
+                            [:<= [:now] :expires-on]]}
+        query-result (querier (sql/format query))]
+    (if (= 1 (count query-result))
+      (-> query-result
+          (nth 0)
+          :id)
+      nil)))
+
 (defmethod ig/init-key ::create
   [_ {:keys [transactor querier]}]
   (fn [req]
@@ -30,3 +42,23 @@
         {:status 409
          :body (str "User " name " already exists; id " user-id)}))))
 
+(defmethod ig/init-key ::login
+  [_ {:keys [transactor querier]}]
+  (fn [req]
+    (let [body-params (:body-params req)
+          name (:name body-params)
+          user-id (user-exists querier name)]
+      (if user-id
+        (let [active-session-id (active-session querier user-id)]
+          (if (not active-session-id)
+            (let [insertion {:insert-into :sessions
+                             :columns [:user-id]
+                             :values [[user-id]]}
+                  _ (transactor (sql/format insertion))
+                  active-session-id (active-session querier user-id)]
+              {:status 200
+               :body {:session active-session-id}})
+            {:status 400
+             :body (str "user " name " already logged in; session " active-session-id)}))
+        {:status 404
+         :body (str "User " name " does not exist")}))))
