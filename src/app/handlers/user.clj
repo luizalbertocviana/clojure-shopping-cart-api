f8c52980e2f1a29306f6432580e1e6d96a639b85
Luiz Alberto do Carmo Viana <luizalbertocviana@gmail.com>
2022-07-19 10:55:36 -0300

    extracts and improves "user exists" query from user creation handler

--- src/app/handlers/user.clj
+++ src/app/handlers/user.clj
@@ -2,17 +2,25 @@
   (:require [integrant.core :as ig]
             [honey.sql :as sql]))
 
+(defn user-exists [querier name]
+  (let [query {:select [:id]
+               :from [:users]
+               :where [:= :name name]}
+        query-result (querier (sql/format query))
+        name-occurrences (count query-result)]
+    (if (= 1 name-occurrences)
+      (-> query-result
+          (nth 0)
+          :id)
+      nil)))
+
 (defmethod ig/init-key ::create
   [_ {:keys [transactor querier]}]
   (fn [req]
     (let [body-params (:body-params req)
           name (:name body-params)
-          query {:select [ [[:count :name]] ]
-                 :from [:users]
-                 :where [:= :name name]}
-          [query-result] (querier (sql/format query))
-          name-occurrences (:count query-result)]
-      (if (zero? name-occurrences)
+          user-id (user-exists querier name)]
+      (if (not user-id)
         (let [insertion {:insert-into :users
                          :columns [:name]
                          :values [[name]]}]
@@ -20,4 +28,5 @@
           {:status 201
            :body (str "User " name " created successfully")})
         {:status 409
-         :body (str "User " name " already exists")}))))
+         :body (str "User " name " already exists; id " user-id)}))))
+
