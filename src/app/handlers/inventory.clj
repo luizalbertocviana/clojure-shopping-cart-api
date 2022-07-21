fbba84dde8ac3739f6b8b2769193d9649a8d2b83
Luiz Alberto do Carmo Viana <luizalbertocviana@gmail.com>
2022-07-21 14:48:52 -0300

    refactors `product-exists` out of `attempt-to-register-product`

--- src/app/handlers/inventory.clj
+++ src/app/handlers/inventory.clj
@@ -2,6 +2,16 @@
   (:require [integrant.core :as ig]
             [honey.sql :as sql]))
 
+(defn product-exists [querier name]
+  (let [query {:select [[[:count :*]]]
+               :from [:inventory]
+               :where [:= :name name]}
+        result (querier (sql/format query))]
+    (-> result
+        (nth 0)
+        :count
+        (= 1))))
+
 (defn register-new-product [transactor name price amount]
   (let [insertion {:insert-into :inventory
                    :columns [:name :price :amount]
@@ -18,18 +28,10 @@
      :body (str "Product " name " removed from inventory")}))
 
 (defn attempt-to-register-product [transactor querier name price amount]
-  (let [product-exists-query {:select [[[:count :*]]]
-                              :from [:inventory]
-                              :where [:= :name name]}
-        product-exists-result (querier (sql/format product-exists-query))
-        product-exists (-> product-exists-result
-                           (nth 0)
-                           :count
-                           (= 1))]
-    (if (not product-exists)
-      (register-new-product transactor name price amount)
-      {:status 409
-       :body (str "Product " name " is already registered")})))
+  (if (not (product-exists querier name))
+    (register-new-product transactor name price amount)
+    {:status 409
+     :body (str "Product " name " is already registered")}))
 
 (defmethod ig/init-key ::post-handler
   [_ {:keys [transactor querier]}]
